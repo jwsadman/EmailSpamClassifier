05ae340c53b37263c3ed65cd471446142a3750f3
jwsadman <jawadsadman996@gmail.com>
2024-02-17 12:51:48 +0600

    EmailSpamClassifier.iml

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pickle
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 import string
 from nltk.stem.porter import PorterStemmer
